23d3d0fba14e6c8986dc4250b270eb9eef1d0f6c
Neha5399 <139036137+Neha5399@users.noreply.github.com>
2023-07-09 12:26:49 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,13 @@
-const mongoose=require('mongoose')
-const DB_URL=process.env.DB_URL
-mongoose.connect(DB_URL).then(()=>{
-    console.log('Connection successful')
-}).catch((err)=>{
-    console.log(err)
+const mongoose = require('mongoose');
+const DB_URL = process.env.DB_URL;
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
+  .then(() => {
+    console.log('Connection successful');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
